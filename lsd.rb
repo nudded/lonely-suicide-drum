fa043cc60ebe22be2d6a14e61be68c035b90e4ff
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-11-09 20:10:34 +0100

    Accept simple post requests

--- lsd.rb
+++ lsd.rb
@@ -1,15 +1,43 @@
+require 'fileutils'
 require 'sinatra/base'
 
 class LSD < Sinatra::Base
 
-  MUSIC_DIRECTORY = File.expand_path '~/test_music/'
+  MUSIC_DIRECTORY = File.expand_path 'test_music'
 
-  get '/songs' do
-    @songs = Dir[MUSIC_DIRECTORY + '/*']
+  # Clean music direcory
+  Dir.glob("#{MUSIC_DIRECTORY}/*").each do |file|
+    File.delete file
   end
 
+  # Start with an empty queue
+  @@queue = Array.new
+  @@id = 0
+
+  # Show song list
+  get '/songs' do
+    @@queue.join " "
+  end
+
+  # Add a song
+  post '/songs' do
+    # Read POST'ed file and put it into the MUSIC_DIRECTORY
+    data = params[:data]
+    tempfile = data[:tempfile]
+    filename = File.join MUSIC_DIRECTORY, @@id.to_s + "-" + data[:filename]
+    File.open filename, "wb" do |file|
+      file.write(tempfile.read)
+    end
+
+    # Add filename to the queue and increment id
+    @@id = @@id + 1
+    @@queue << filename
+    @@queue.to_s
+  end
+
+  # Home
   get '/' do 
-    "awesome" 
+    redirect '/songs'
   end
 
 end
